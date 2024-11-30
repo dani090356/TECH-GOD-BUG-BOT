f5fe4f133e98dde5a73177b4a0550e2c1c7b633f
dani090356 <m9233917@gmail.com>
2024-11-30 22:53:36 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
-global.ytname = "YT: Tech-God"
+global.ownernomer = "+923468927192"
+global.ownername = "🐛Hyper"
+global.ytname = "Hyper"
 global.socialm = "GitHub: techgod143"
-global.location = "India, Up, Rampur"
+global.location = "Karachi Pakistan"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
+global.ownernumber = '+923468927192'  //creator number
+global.ownername = '🐛Hyper' //owner name
 global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
